64f5df3c2c5990193a3f38f318e9afbdfbba3de8
zhangyingwei <zhangyw_001@126.com>
2016-07-31 22:10:22 +0800

    a

--- src/test/java/com/zhangyw/jianshu/http/HttpTest.java
+++ src/test/java/com/zhangyw/jianshu/http/HttpTest.java
@@ -3,13 +3,17 @@ package com.zhangyw.jianshu.http;
 import java.io.IOException;
 import java.util.Arrays;
 
+import net.sf.json.JSONObject;
+
 import org.apache.commons.httpclient.methods.GetMethod;
+
+import com.zhangyw.superhttp.http.Http;
 
 public class HttpTest {
 	public static void main(String[] args) throws IOException {
-		GetMethod get = HttpHandler.doGet("http://www.jianshu.com/search/do?q=%E6%84%88%E5%A7%91%E5%A8%98&page=1&type=users");
-		System.out.println(Arrays.toString(get.getRequestHeaders()));
-		System.out.println(get.getStatusCode());
-		System.out.println(get.getResponseBodyAsString());
+		GetMethod get = Http.getIS().get("http://www.jianshu.com/search/do?q=%E6%84%88%E5%A7%91%E5%A8%98&page=1&type=users",Http.CLIENT_TYPE_BOWSER);
+		String result = get.getResponseBodyAsString();
+		JSONObject json = JSONObject.fromObject(result);
+		System.out.println(json.getJSONArray("entries"));
 	}
 }
